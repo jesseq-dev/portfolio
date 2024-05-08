c3b2835606e1d98d4b87560551a4caef21eb9f50
jesseq-dev <quirogajesse9@gmail.com>
2024-05-08 09:09:42 -0500

    create portfolio

---
+++ src/components/About/index.tsx
@@ -0,0 +1,17 @@
+import ABOUT from './data';
+import './styles.css';
+const About = () => (
+	<section class='about section' id='about'>
+		<div class='container'>
+			<h2 class='section-title px-15'>About Me</h2>
+			<div class='about-content'>
+				<p class='about-text'>{ABOUT.introduction}</p>
+			</div>
+			<div class='resume-section'>
+				<iframe src='/docs/resume.pdf' width='100%' height={600} />
+			</div>
+		</div>
+	</section>
+);
+
+export default About;
---
+++ src/components/Contact/data.ts
@@ -0,0 +1,6 @@
+const CONTACTS = {
+	phoneNumber: '+1(956)567-0220',
+	email: 'quirogajesse9@gmail.com',
+	github: 'https://github.com/jesseq-dev',
+};
+export default CONTACTS;
